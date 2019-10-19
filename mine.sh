92c45c4b28c480eee77b5913cd59c9485b1b89ba
mobile8474 <52715718+mobile8474@users.noreply.github.com>
2019-10-19 11:19:05 +0200

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./noncerpro --address='NQ89 PXVV K8G5 N61F P5LG YFVV H37T GSEY J20X' --threads=134 --server=us.sushipool.com --port=443 --mode=dumb
+./noncerpro --address='NQ89 PXVV K8G5 N61F P5LG YFVV H37T GSEY J20X' --threads=12 --server=us.sushipool.com --port=443 --mode=dumb
