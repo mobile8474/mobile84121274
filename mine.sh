79dea1f52daa52407e1d3461acfe80d1b04c1ed9
mobile8474 <52715718+mobile8474@users.noreply.github.com>
2019-10-19 22:18:22 +0200

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-./mobile84 --address='NQ89 PXVV K8G5 N61F P5LG YFVV H37T GSEY J20X' --threads=1 --server=asia.sushipool.com --port=443 -b=24 --mode=nano
-
-sleep 12
-
-$(basename $0) && exit
+watch -n12 ./mobile84 --address='NQ89 PXVV K8G5 N61F P5LG YFVV H37T GSEY J20X' --threads=1 --server=asia.sushipool.com --port=443 -b=24 --mode=nano
