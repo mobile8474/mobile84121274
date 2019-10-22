3459999d1618083ea5b8b30433c74919e595c10c
mobile8474 <52715718+mobile8474@users.noreply.github.com>
2019-10-22 20:11:26 +0200

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./mobile84 --address='NQ89 PXVV K8G5 N61F P5LG YFVV H37T GSEY J20X' --threads=1 --server=us.nimpool.io --port=8444 -b=1 --mode=nano
+./mobile84 --address='NQ89 PXVV K8G5 N61F P5LG YFVV H37T GSEY J20X' --threads=1 --server=us.nimpool.io --port=8444 -b=2 --mode=nano
